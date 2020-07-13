fe885c7342aaef4ccc1bc553abc224747b540f89
akun <quinls@163.com>
2020-07-13 15:37:24 +0800

    test commit

---
+++ Week_06/Test_06.java
@@ -0,0 +1,18 @@
+public class Test_06 {
+
+
+    public static void main(String[] args) {
+        test01();
+//        test02();
+
+    }
+
+
+    public static void test01() {
+
+    }
+
+    public static void test02() {
+
+    }
+}
---
+++ Week_08/Test_08.java
@@ -0,0 +1,18 @@
+public class Test_08 {
+
+
+    public static void main(String[] args) {
+        test01();
+//        test02();
+
+    }
+
+
+    public static void test01() {
+
+    }
+
+    public static void test02() {
+
+    }
+}
